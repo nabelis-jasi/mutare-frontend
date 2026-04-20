da088a0cea6687dd2fe066b19c69be47b499eeeb
nabelis-jasi <nabelisj65@gmail.com>
2026-04-20 14:49:58 +0200

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,10 +1,20 @@
-// components/header.js
+// components/header.js - With online logo placeholder
+
 export default {
     render() {
         return `
             <div class="panel-header">
-                <h1>MUTARE WASTEWATER SYSTEM</h1>
-               </div>
+                <div class="logo-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Flag_of_Zimbabwe.svg/1200px-Flag_of_Zimbabwe.svg.png" 
+                         alt="City of Mutare Logo" 
+                         class="city-logo"
+                         onerror="this.src='https://placehold.co/60x60/228B22/white?text=MUTARE'">
+                    <div class="logo-text">
+                        <h1>🏙️ CITY OF MUTARE</h1>
+                        <p>WASTEWATER SYSTEM</p>
+                    </div>
+                </div>
+            </div>
         `;
     }
 };
