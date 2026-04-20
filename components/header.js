a1a7765383248c03e6b7e9825d6785cdf6e17e83
nabelis-jasi <nabelisj65@gmail.com>
2026-04-20 15:04:09 +0200

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,17 +1,17 @@
-// components/header.js - With online logo placeholder
+// components/header.js - Dashboard Header with City of Mutare Logo
 
 export default {
     render() {
         return `
             <div class="panel-header">
                 <div class="logo-container">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Flag_of_Zimbabwe.svg/1200px-Flag_of_Zimbabwe.svg.png" 
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Mutare_City_logo_2023.png" 
                          alt="City of Mutare Logo" 
                          class="city-logo"
-                         onerror="this.src='https://placehold.co/60x60/228B22/white?text=MUTARE'">
+                         onerror="this.onerror=null; this.src='https://placehold.co/60x60/228B22/white?text=MUTARE'">
                     <div class="logo-text">
-                        <h1>🏙️ CITY OF MUTARE</h1>
-                        <p>WASTEWATER SYSTEM</p>
+                        <h1>CITY OF MUTARE</h1>
+                        <p>WASTEWATER MANAGEMENT SYSTEM</p>
                     </div>
                 </div>
             </div>
