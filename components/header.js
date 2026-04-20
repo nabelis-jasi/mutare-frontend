77d1e68fcfea567c118ed758a48f0960a5f61f7e
nabelis-jasi <nabelisj65@gmail.com>
2026-04-20 10:39:33 +0200

    Create header.js

---
+++ components/header.js
@@ -0,0 +1,11 @@
+// components/header.js
+export default {
+    render() {
+        return `
+            <div class="panel-header">
+                <h1>MUTARE SEWER ENGINEER</h1>
+                <p>Click to Filter | No Query Needed</p>
+            </div>
+        `;
+    }
+};
